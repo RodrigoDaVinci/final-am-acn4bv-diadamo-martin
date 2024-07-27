7f3e4e139d747b45f87f9b475bde810dfaffafba
RodrigoDaVinci <rodrigo.martin@davinci.edu.ar>
2024-07-27 12:53:16 -0300

    feat: Charlas seguridad

--- app/src/main/java/com/example/parcial_2_am_acn4bv_diadamo_martin/CharlasSeguridadActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_diadamo_martin/CharlasSeguridadActivity.java
@@ -1,24 +1,61 @@
 package com.example.parcial_2_am_acn4bv_diadamo_martin;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class CharlasSeguridadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_charlas_seguridad);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button buttonLogout = findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
         });
+    }
+
+    public void openVideo1(View view) {
+        openVideo("https://www.youtube.com/watch?v=EGIuCyrhXt4");
+    }
+
+    public void openVideo2(View view) {
+        openVideo("https://www.youtube.com/watch?v=HQXfZJF0Vag");
+    }
+
+    public void openVideo3(View view) {
+        openVideo("https://www.youtube.com/watch?v=zvF-v53Xr8E");
+    }
+
+    public void openVideo4(View view) {
+        openVideo("https://www.youtube.com/watch?v=iTHaM4XTQkg");
+    }
+
+    public void openVideo5(View view) {
+        openVideo("https://www.youtube.com/watch?v=lpj3A2Tj2ag");
+    }
+
+    public void openVideo6(View view) {
+        openVideo("https://www.youtube.com/watch?v=_ewq-nceFX0");
+    }
+
+    private void openVideo(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+
+    private void logout() {
+        Intent intent = new Intent(CharlasSeguridadActivity.this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
